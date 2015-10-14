30aa2c85d82623d1661d72a8f33b317cdabb6c1f
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2015-10-14 16:49:41 +0100

    Changing font to a global variable

--- PresentPicker/src/application/Form.java
+++ PresentPicker/src/application/Form.java
@@ -3,18 +3,22 @@ package application;
 import java.awt.*;
 import java.awt.event.*;
 
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 import gifted.Gifted;
 
 public class Form extends JFrame {
 	/* Form attributes */
+	private JPanel pane;
 	private JLabel firstNameLabel;
 	private JLabel lastNameLabel;
 	private JLabel genderLabel;
@@ -41,23 +45,30 @@ public class Form extends JFrame {
 	private JButton searchButton;
 	
 	private String genders[] = {"", "Male", "Female"};
+	public final String FONT_TITTLE = "Andalus";
+	public final String FONT_BODY = "Sylfaen";
+	public final String FONT_BUTTON = "Sylfaen";
 	
 	Gifted gifted = new Gifted();
 	
 	public static void main(String[] args) {
-		/* Create the window - size = 1000x860 and start = (400, 100) */
-		Form frame = new Form();
-		frame.setBounds(400, 100, 1000, 860);
-		frame.setTitle("Present Picker");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		Form frame = new Form();
 		/* Make the window visible */
 		frame.setVisible(true);
 	}
 	
 	public Form() {
-		Container pane = getContentPane();
-		/* Set layout as null means set it as AbsolutLayout */
+		
+		setBounds(400, 100, 1000, 860);
+		setTitle("Present Picker");
+		/* Disable resize */
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		pane = new JPanel();
+		pane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(pane);
 		pane.setLayout(null);
 		
 		appIconLabel = new JLabel("");
@@ -66,118 +77,118 @@ public class Form extends JFrame {
 		pane.add(appIconLabel);
 		
 		appNameLabel = new JLabel("PRESENT PICKER");
-		appNameLabel.setFont(new Font("Andalus", Font.BOLD, 24));
+		appNameLabel.setFont(new Font(FONT_TITTLE, Font.BOLD, 24));
 		appNameLabel.setBounds(100, 40, 250, 30);
 		pane.add(appNameLabel);
 		
 		formDescriptionLabel = new JLabel("We need some information regarding the gifted...");
-		formDescriptionLabel.setFont(new Font("Andalus", Font.BOLD, 16));
+		formDescriptionLabel.setFont(new Font(FONT_BODY, Font.BOLD, 16));
 		formDescriptionLabel.setBounds(50, 130, 380, 30);
 		pane.add(formDescriptionLabel);
 		
 		firstNameLabel = new JLabel("First Name: ");
-		firstNameLabel.setFont(new Font("Andalus", Font.BOLD, 16));
+		firstNameLabel.setFont(new Font(FONT_BODY, Font.BOLD, 16));
 		firstNameLabel.setBounds(50, 180, 100, 30);
 		pane.add(firstNameLabel);
 		
 		firstNameTextField = new JTextField();
-		firstNameTextField.setFont(new Font("Andalus", Font.PLAIN, 16));
+		firstNameTextField.setFont(new Font(FONT_BODY, Font.PLAIN, 16));
 		firstNameTextField.setBounds(170, 180, 300, 30);
 		pane.add(firstNameTextField);
 		
 		lastNameLabel = new JLabel("Last Name: ");
-		lastNameLabel.setFont(new Font("Andalus", Font.BOLD, 16));
+		lastNameLabel.setFont(new Font(FONT_BODY, Font.BOLD, 16));
 		lastNameLabel.setBounds(520, 180, 100, 30);
 		pane.add(lastNameLabel);
 		
 		lastNameTextField = new JTextField();
-		lastNameTextField.setFont(new Font("Andalus", Font.PLAIN, 16));
+		lastNameTextField.setFont(new Font(FONT_BODY, Font.PLAIN, 16));
 		lastNameTextField.setBounds(620, 180, 300, 30);
 		pane.add(lastNameTextField);
 		
 		genderLabel = new JLabel("Gender: ");
-		genderLabel.setFont(new Font("Andalus", Font.BOLD, 16));
+		genderLabel.setFont(new Font(FONT_BODY, Font.BOLD, 16));
 		genderLabel.setBounds(50, 250, 100, 30);
 		pane.add(genderLabel);
 		
 		genderComboBox = new JComboBox<String>(genders);
-		genderComboBox.setFont(new Font("Andalus", Font.PLAIN, 16));
+		genderComboBox.setFont(new Font(FONT_BODY, Font.PLAIN, 16));
 		genderComboBox.setBounds(170, 250, 300, 30);
 		pane.add(genderComboBox);
 		
 		ageLabel = new JLabel("Age: ");
-		ageLabel.setFont(new Font("Andalus", Font.BOLD, 16));
+		ageLabel.setFont(new Font(FONT_BODY, Font.BOLD, 16));
 		ageLabel.setBounds(520, 250, 100, 30);
 		pane.add(ageLabel);
 		
 		ageTextField = new JTextField();
-		ageTextField.setFont(new Font("Andalus", Font.PLAIN, 16));
+		ageTextField.setFont(new Font(FONT_BODY, Font.PLAIN, 16));
 		ageTextField.setBounds(620, 250, 100, 30);
 		pane.add(ageTextField);
 		
 		relationshipLabel = new JLabel("Relationship with you: ");
-		relationshipLabel.setFont(new Font("Andalus", Font.BOLD, 16));
+		relationshipLabel.setFont(new Font(FONT_BODY, Font.BOLD, 16));
 		relationshipLabel.setBounds(50, 330, 200, 30);
 		pane.add(relationshipLabel);
 		
 		relationshipTextField = new JTextField();
-		relationshipTextField.setFont(new Font("Andalus", Font.PLAIN, 16));
+		relationshipTextField.setFont(new Font(FONT_BODY, Font.PLAIN, 16));
 		relationshipTextField.setBounds(240, 330, 230, 30);
 		pane.add(relationshipTextField);
 		
 		interestLabel = new JLabel("Interests: ");
-		interestLabel.setFont(new Font("Andalus", Font.BOLD, 16));
+		interestLabel.setFont(new Font(FONT_BODY, Font.BOLD, 16));
 		interestLabel.setBounds(50, 410, 150, 30);
 		pane.add(interestLabel);
 		
 		movieBookCheckBox = new JCheckBox("Movies/Books");
-		movieBookCheckBox.setFont(new Font("Andalus", Font.PLAIN, 16));
+		movieBookCheckBox.setFont(new Font(FONT_BODY, Font.PLAIN, 16));
 		movieBookCheckBox.setBounds(50, 480, 150, 30);
 		pane.add(movieBookCheckBox);
 		
 		sportCheckBox = new JCheckBox("Sports");
-		sportCheckBox.setFont(new Font("Andalus", Font.PLAIN, 16));
+		sportCheckBox.setFont(new Font(FONT_BODY, Font.PLAIN, 16));
 		sportCheckBox.setBounds(50, 530, 150, 30);
 		pane.add(sportCheckBox);
 		
 		religionCheckBox = new JCheckBox("Religion");
-		religionCheckBox.setFont(new Font("Andalus", Font.PLAIN, 16));
+		religionCheckBox.setFont(new Font(FONT_BODY, Font.PLAIN, 16));
 		religionCheckBox.setBounds(50, 580, 150, 30);
 		pane.add(religionCheckBox);
 		
 		kidsCheckBox = new JCheckBox("Kids");
-		kidsCheckBox.setFont(new Font("Andalus", Font.PLAIN, 16));
+		kidsCheckBox.setFont(new Font(FONT_BODY, Font.PLAIN, 16));
 		kidsCheckBox.setBounds(350, 480, 150, 30);
 		pane.add(kidsCheckBox);
 		
 		outdoorCheckBox = new JCheckBox("Outdoor");
-		outdoorCheckBox.setFont(new Font("Andalus", Font.PLAIN, 16));
+		outdoorCheckBox.setFont(new Font(FONT_BODY, Font.PLAIN, 16));
 		outdoorCheckBox.setBounds(350, 530, 150, 30);
 		pane.add(outdoorCheckBox);
 		
 		houseCheckBox = new JCheckBox("House");
-		houseCheckBox.setFont(new Font("Andalus", Font.PLAIN, 16));
+		houseCheckBox.setFont(new Font(FONT_BODY, Font.PLAIN, 16));
 		houseCheckBox.setBounds(350, 580, 150, 30);
 		pane.add(houseCheckBox);
 		
 		gameCheckBox = new JCheckBox("Games");
-		gameCheckBox.setFont(new Font("Andalus", Font.PLAIN, 16));
+		gameCheckBox.setFont(new Font(FONT_BODY, Font.PLAIN, 16));
 		gameCheckBox.setBounds(650, 480, 150, 30);
 		pane.add(gameCheckBox);
 		
 		drinkCheckBox = new JCheckBox("Drink Enthusiastic");
-		drinkCheckBox.setFont(new Font("Andalus", Font.PLAIN, 16));
+		drinkCheckBox.setFont(new Font(FONT_BODY, Font.PLAIN, 16));
 		drinkCheckBox.setBounds(650, 530, 150, 30);
 		pane.add(drinkCheckBox);
 		
 		musicCheckBox = new JCheckBox("Music");
-		musicCheckBox.setFont(new Font("Andalus", Font.PLAIN, 16));
+		musicCheckBox.setFont(new Font(FONT_BODY, Font.PLAIN, 16));
 		musicCheckBox.setBounds(650, 580, 150, 30);
 		pane.add(musicCheckBox);
 		
 		searchButton = new JButton("SEARCH");
-		searchButton.setFont(new Font("Andalus", Font.PLAIN, 16));
-		searchButton.setBounds(390, 700, 150, 30);
+		searchButton.setFont(new Font(FONT_BUTTON, Font.PLAIN, 16));
+		searchButton.setBounds(520, 700, 150, 30);
 		
 		searchButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
@@ -187,7 +198,8 @@ public class Form extends JFrame {
 				gifted.setAgeGifted(Integer.parseInt(ageTextField.getText()));
 				gifted.setGenderGifted(genderComboBox.getSelectedItem().toString());
 				
-				System.out.println("Name: " + gifted.getFirstNameGifted() + " " + gifted.getLastNameGifted());
+				System.out.println("Name: " + gifted.getFirstNameGifted() + " " + 
+						gifted.getLastNameGifted());
 				System.out.println("Gender: " + gifted.getGenderGifted());
 				System.out.println("Age: " + gifted.getAgeGifted());
 				System.out.println("Relationship: " + gifted.getRelationshipGifted());
--- PresentPicker/src/application/Welcome.java
+++ PresentPicker/src/application/Welcome.java
@@ -7,41 +7,55 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 public class Welcome extends JFrame {
+	
 	/* Welcome attributes */
+	private JPanel pane;
 	private JLabel welcomeLabel;
 	private JLabel appNameLabel;
 	private JLabel appImageLabel;
 	private JButton startButton;
 	
+	public final String FONT_TITTLE = "Andalus";
+	public final String FONT_BODY = "Sylfaen";
+	public final String FONT_BUTTON = "Sylfaen";
+	
 	public static void main(String[] args) {
-		/* Create the window - size = 1000x860 and start = (400, 100) */
-		Welcome frame = new Welcome();
-		frame.setBounds(400, 100, 1000, 860);
-		frame.setTitle("Present Picker");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		Welcome frame = new Welcome();
 		/* Make the window visible */
 		frame.setVisible(true);
 	}
 	
 	public Welcome() {
-		Container pane = getContentPane();
+		
+		/* Create the window - size = 1000x860 and start = (400, 100) */
+		setBounds(400, 100, 1000, 860);
+		setTitle("Present Picker");
+		/* Disable resize */
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		pane = new JPanel();
+		pane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(pane);
 		/* Set layout as null means set it as AbsolutLayout */
 		pane.setLayout(null);
 		
 		/* Create a label (Welcome to) and add it into the panel */
 		welcomeLabel = new JLabel("WELCOME TO");
 		/* Set font attributes to label */
-		welcomeLabel.setFont(new Font("Andalus", Font.BOLD, 20));
+		welcomeLabel.setFont(new Font(FONT_TITTLE, Font.BOLD, 20));
 		/* Usage of AbsolutLayout means that you need to specify where the component is */
 		welcomeLabel.setBounds(690, 280, 150, 30);
 		pane.add(welcomeLabel);
 		
 		/* Create a label (Present Picker) and add it into the panel */
 		appNameLabel = new JLabel("PRESENT PICKER");
-		appNameLabel.setFont(new Font("Andalus", Font.BOLD, 40));
+		appNameLabel.setFont(new Font(FONT_TITTLE, Font.BOLD, 40));
 		appNameLabel.setForeground(new Color(0, 102, 255));
 		appNameLabel.setBounds(600, 320, 320, 30);
 		pane.add(appNameLabel);
@@ -55,7 +69,7 @@ public class Welcome extends JFrame {
 		
 		/* Create a button (Start) and add it into the panel */
 		startButton = new JButton("START");
-		startButton.setFont(new Font("Andalus", Font.PLAIN, 16));
+		startButton.setFont(new Font(FONT_BUTTON, Font.PLAIN, 16));
 		startButton.setBounds(690, 500, 120, 40);
 		
 		/* Add an action to the button using anonymous inner class */
